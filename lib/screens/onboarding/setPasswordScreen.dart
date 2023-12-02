a7e0fc7eb1bffb38f9e893526fa71b5a382c3973
ShRudra88 <salhawathosenrudra503@gmail.com>
2023-12-02 15:11:54 +0600

    update

--- lib/screens/onboarding/setPasswordScreen.dart
+++ lib/screens/onboarding/setPasswordScreen.dart
@@ -1,7 +1,9 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:task_manager_project/utility/utility.dart';
 
+import '../../api/apiClient.dart';
 import '../../style/style.dart';
 
 class setPasswordScreen extends StatefulWidget {
@@ -12,6 +14,55 @@ class setPasswordScreen extends StatefulWidget {
 }
 
 class _setPasswordScreenState extends State<setPasswordScreen> {
+
+  Map<String, String> FormValues = {"email":"", "OTP":"","password":"", "cpassword":""}
+  bool Loading = false;
+
+  @override
+  void initState() {
+    callStoreDate();
+    super.initState();
+  }
+
+  callStoreDate()async{
+    String? OTP = await ReadUserData('OTPVerification');
+    String? Email = await ReadUserData("EmailVerification");
+
+    InputOnChange("email", Email);
+    InputOnChange("OTP", OTP);
+  }
+
+  InputOnChange(MapKey, TextValue)
+  {
+    setState(() {
+      FormValues.update(MapKey, (value) => TextValue);
+    });
+  }
+
+  FormOnSubmit() async {
+    if (FormValues['password']!.length == 0) {
+      ErrorToast('password required');
+    } else if (FormValues['password']!= FormValues['cpassword']) {
+      ErrorToast('Confirm password should be same');
+    } else {
+
+      setState(() {
+        Loading = true;
+      });
+
+      bool res = await SetPasswordRequest(FormValues);
+
+      if (res == true) {
+        Navigator.pushNamedAndRemoveUntil(
+            context, '/login', (route) => false);
+      } else {
+        setState(() {
+          Loading = false;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +88,7 @@ class _setPasswordScreenState extends State<setPasswordScreen> {
                     style: AppButtonStyle(),
                     child: SuccessButtonChild('Conform'),
                     onPressed: (){
-
+                      FormOnSubmit()
                     },
                   ),
                 )
