1af9367fe35329cc46361f2e33ab9210b5c42065
ShRudra88 <salhawathosenrudra503@gmail.com>
2023-12-16 02:17:57 +0600

    update

---
+++ lib/data_network_caller/netword_caller.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:task_manager_project/app.dart';
+import 'package:task_manager_project/ui/controllers/auth_controller.dart';
+import 'package:task_manager_project/ui/screens/login_screen.dart';
+
+import 'network_response.dart';
+
+class NetworkCaller {
+  Future<NetworkResponse> postRequest(String url,
+      {Map<String, dynamic>? body, bool isLogin = false}) async {
+    try {
+      log(url);
+      log(body.toString());
+      final Response response =
+      await post(Uri.parse(url), body: jsonEncode(body), headers: {
+        'Content-type': 'Application/json',
+        'token' : AuthController.token.toString(),
+      });
+      log(response.headers.toString());
+      log(response.statusCode.toString());
+      log(response.body.toString());
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+          isSuccess: true,
+          jsonResponse: jsonDecode(response.body),
+          statusCode: 200,
+        );
+      } else if (response.statusCode == 401) {
+        if (isLogin == false) {
+          backToLogin();
+        }
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          jsonResponse: jsonDecode(response.body),
+        );
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          jsonResponse: jsonDecode(response.body),
+        );
+      }
+    } catch (e) {
+      return NetworkResponse(isSuccess: false, errorMessage: e.toString());
+    }
+  }
+
+  Future<NetworkResponse> getRequest(String url) async {
+    try {
+      log(url);
+      final Response response =
+      await get(Uri.parse(url), headers: {
+        'Content-type': 'Application/json',
+        'token' : AuthController.token.toString(),
+      });
+      log(response.headers.toString());
+      log(response.statusCode.toString());
+      log(response.body.toString());
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+          isSuccess: true,
+          jsonResponse: jsonDecode(response.body),
+          statusCode: 200,
+        );
+      } else if (response.statusCode == 401) {
+        backToLogin();
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          jsonResponse: jsonDecode(response.body),
+        );
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          statusCode: response.statusCode,
+          jsonResponse: jsonDecode(response.body),
+        );
+      }
+    } catch (e) {
+      return NetworkResponse(isSuccess: false, errorMessage: e.toString());
+    }
+  }
+
+  Future<void> backToLogin() async {
+    await AuthController.clearAuthData();
+    Navigator.pushAndRemoveUntil(
+        TaskManagerApp.navigationKey.currentContext!,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+            (route) => false);
+  }
+}
--- lib/ui/screens/edit_profile_screen.dart
+++ lib/ui/screens/edit_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_project/ui/widgets/body_background.dart';
 import 'package:task_manager_project/ui/widgets/profile_summary_card.dart';
 
 class EditProfileScreen extends StatefulWidget {
@@ -10,21 +11,62 @@ class EditProfileScreen extends StatefulWidget {
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(context) => Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            ProfileSummaryCard(
+            const ProfileSummaryCard(
               enableOnTap: false,
             ),
-            Expanded(child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children: [
+            Expanded(child: BodyBackground(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 32,),
+                      Text('Update Profile',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      const SizedBox(height: 16,),
+                      PhotoPickerField(),const SizedBox(height: 8,),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: 'Email'
+                        ),
+                      ),const SizedBox(height: 8,),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: 'First Name'
+                        ),
+                      ),const SizedBox(height: 8,),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: 'Last Name'
+                        ),
+                      ),const SizedBox(height: 8,),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: 'Mobile'
+                        ),
+                      ),const SizedBox(height: 8,),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: 'Password'
+                        ),
+                      ),
+                      const SizedBox(height: 8,),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: (){
 
-                  ],
+                          },
+                          child: const Icon(Icons.arrow_circle_right_outlined),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),)
@@ -32,5 +74,41 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
         ),
       ),
     );
+
+  Container PhotoPickerField() {
+    return Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8)
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              flex: 1,
+                              child: Container(
+                                height: 50,
+                                decoration: const BoxDecoration(
+                                    color: Colors.grey,
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(8),
+                                      bottomLeft: Radius.circular(8),
+                                    )
+                                ),
+                                alignment: Alignment.center,
+                                child: const Text('photo', style: TextStyle(
+                                  color: Colors.white
+                                ),),
+
+                              )),
+                          Expanded(
+                              flex: 3,
+                              child: Container(
+                                padding: const EdgeInsets.only(left: 16),
+                                child: const Text('Empty'),
+                              )),
+                        ],
+                      ),
+                    );
   }
 }
